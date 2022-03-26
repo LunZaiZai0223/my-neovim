e949840c80447550462463f3313629a2a1ad2751
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-03-26 17:34:56 +0800

    add nvim-tree git ,diagnostic and mapping config

--- lua/lun/nvim-tree.lua
+++ lua/lun/nvim-tree.lua
@@ -1,17 +1,42 @@
 -- must be set before calling require
 vim.g.nvim_tree_indent_markers = 1 -- this option shows indent markers when folders are open
 
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
 require('nvim-tree').setup {
-    auto_close = true,
+  auto_close = true,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
     update_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_cwd = true,
-      ignore_list = {},
+    ignore_list = {},
+  },
+  view = {
+    auto_resize = true,
+    width = 25,
+    mappings = {
+      custom_only = false,
+      list = {
+        -- user mappings go here
+      { key = "v", cb = tree_cb("vsplit") },
+      },
     },
-    view = {
-        auto_resize = true,
-    }
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  }
 }
 
 vim.api.nvim_set_keymap('n', '<space>t', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
