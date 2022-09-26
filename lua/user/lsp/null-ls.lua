c5473332d1a461fc6535859e5bc8dafd07bcdb66
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-09-26 22:13:34 +0800

    fix(null-ls): save format with prettier or eslint_d (if there is an eslint config)

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,23 +3,56 @@ if not null_ls_status_ok then
   return
 end
 
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 -- 公司會用的 prettier setting:
 -- "--print-width 120", "--arrow-parens avoid", "--prose-wrap=preserve", "--trailing-comma=es5"
 null_ls.setup {
-  -- 如果沒有引入 eslint 就先關掉
-  -- diagnostics.eslint_d,
   debug = false,
   sources = {
     -- formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
     formatting.prettier.with({
       extra_args = {
         "--single-quote", "--print-width=120", "--prose-wrap=preserve", "--trailing-comma=es5", "--arrow-parens=avoid"
-      }
+      },
+      filetypes = {
+        'typescriptreact',
+        'typescript',
+        'javascriptreact',
+        'javascript',
+        'svelte',
+        'json',
+        'jsonc',
+        'css',
+        'less',
+        'scss',
+        'html',
+        'yaml',
+      },
     }),
+
+    -- 有 eslint 就開啟
+    -- diagnostics.eslint_d.with({
+    --   diagnostics_format = '[eslint] #{m}\n(#{c})'
+    -- }),
+    -- null_ls.builtins.formatting.eslint_d,
+    -- null_ls.builtins.diagnostics.eslint_d,
+    -- null_ls.builtins.code_actions.eslint_d,
   },
+  -- auto on save
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup_format,
+        buffer = 0,
+        callback = function() vim.lsp.buf.formatting_seq_sync() end
+      })
+    end
+  end,
 }
